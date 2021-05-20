df41953a7a37c8b52ee4b680287357a1b75fe283
George Shvab <62070431+GeorgeShvab@users.noreply.github.com>
2021-05-21 00:22:31 +0300

    Add files via upload

--- works/konstruct/templates/scripts/script.js
+++ works/konstruct/templates/scripts/script.js
@@ -1,1 +1,10 @@
-
+window.onload = function() {	
+	$(".header__button").click(function() {
+		$(".header__nav-mobile").css("display", "block");
+		$("body").css("overflow", "hidden");
+	});
+	$(".menu__button").click(function() {
+		$(".header__nav-mobile").css("display", "none");
+		$("body").css("overflow", "auto");
+	});
+}
