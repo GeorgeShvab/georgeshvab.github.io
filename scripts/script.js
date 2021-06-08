51594a18d597057b77f3a5da14b16366862e6054
George Shvab <62070431+GeorgeShvab@users.noreply.github.com>
2021-06-08 17:59:47 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,40 @@
 window.onload = function onload() {
 	$("#header__about-me").click(function(){
-		scroll_to_about_me();
+		scroll_to_section("about-me");
 	})
 	$("#header__skills").click(function(){
-		scroll_to_skills();
+		scroll_to_section("skills");
 	})
-	$("#header__portfolio").click(function(){
-		scroll_to_portfolio();
+	$("#header__portfolio, .intro__button").click(function(){
+		scroll_to_section("portfolio");
 	})
 	$("#header__contacts").click(function(){
-		scroll_to_contacts();
+		scroll_to_section("contacts");
 	})
 	$(".btn").click(function(){
 		alert($(".skills").offset().top.toFixed());
 		alert($("html").scrollTop().toFixed());
 	})
 	$(window).scroll(function() {
-  		scroll_animation("skills__wrapper", "_active");
-  		scroll_animation("skills__title", "_active");
-  		scroll_animation("about-me__content-paragraph-3", "_active");
-  		scroll_animation("about-me__content-paragraph-2", "_active");
-  		scroll_animation("about-me__content-paragraph-1", "_active");
-  		scroll_animation("about-me__content-subtitle", "_active");
-  		scroll_animation("about-me__illustration", "_active-image");
+  		scroll_event("skills__wrapper", "_active");
+  		scroll_event("skills__title", "_active");
+  		scroll_event("about-me__content-paragraph-3", "_active");
+  		scroll_event("about-me__content-paragraph-2", "_active");
+  		scroll_event("about-me__content-paragraph-1", "_active");
+  		scroll_event("about-me__content-subtitle", "_active");
+  		scroll_event("about-me__title", "_active");
+  		scroll_event("about-me__illustration", "_active-image");
 	});
 }
 
-function scroll_animation(old_class, new_class) {
+function scroll_event(old_class, new_class) {
   		if ($("html").scrollTop() > $('.' + old_class).offset().top - $(window).height() - 100) {
 			$('.' + old_class).addClass(new_class);
   		}
 }
 
-function scroll_to_about_me() {
+function scroll_to_section(section_name) {
 	$('html, body').animate({
-        scrollTop: $(".about-me").offset().top
-    }, 2000);
-}
-function scroll_to_skills() {
-	$('html, body').animate({
-        scrollTop: $(".skills").offset().top
-    }, 2000);
-}
-function scroll_to_portfolio() {
-	$('html, body').animate({
-        scrollTop: $(".portfolio").offset().top
-    }, 2000);
-}
-function scroll_to_contacts() {
-	$('html, body').animate({
-        scrollTop: $(".contacts").offset().top
+        scrollTop: $('.' + section_name).offset().top
     }, 2000);
 }
