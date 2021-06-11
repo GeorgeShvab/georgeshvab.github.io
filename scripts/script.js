fc45c7079a9a77d3064cb96888e78759b3cb6bbb
George Shvab <62070431+GeorgeShvab@users.noreply.github.com>
2021-06-12 00:49:05 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -5,11 +5,8 @@ window.onload = function onload() {
 	$("#header__skills").click(function(){
 		scroll_to_section("skills");
 	})
-	$("#header__portfolio, .intro__button").click(function(){
+	$("#header__portfolio").click(function(){
 		scroll_to_section("portfolio");
-	})
-	$("#header__contacts").click(function(){
-		scroll_to_section("contacts");
 	})
 	$(window).scroll(function() {
   		scroll_event("skills__wrapper", "_active");
@@ -27,6 +24,9 @@ window.onload = function onload() {
   		scroll_event("portfolio__block-1", "_active");
   		scroll_event("portfolio__block-2", "_active");
 	});
+	$(".menu__button").click(function(){
+		open_menu();
+	})
 }
 
 function scroll_event(old_class, new_class) {
@@ -34,9 +34,33 @@ function scroll_event(old_class, new_class) {
 			$('.' + old_class).addClass(new_class);
   		}
 }
-
 function scroll_to_section(section_name) {
 	$('html, body').animate({
         scrollTop: $('.' + section_name).offset().top
     }, 2000);
+}
+function close_menu() {
+	$(".menu").css("left", "100vw");
+	$(".menu__items-inactive").removeClass("menu__items-active");
+	$("html").css("overflow", "scroll");
+}
+function open_menu() {
+	$(".menu").css("left", "25vw");
+	$("html").css("overflow", "hidden");
+	$(".menu__items-inactive").addClass("menu__items-active");
+	$("#menu__about-me").click(function(){
+		scroll_to_section("about-me");
+		close_menu();
+	})
+	$("#menu__skills").click(function(){
+		scroll_to_section("skills");
+		close_menu();
+	})
+	$("#menu__portfolio").click(function(){
+		scroll_to_section("portfolio");
+		close_menu();
+	})
+	$(".intro").click(function(){
+		close_menu();
+	})
 }
