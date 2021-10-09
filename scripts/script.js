55fd412e0a489e3561a6cccc8f8cf4c97ce87bfa
George Shvab <62070431+GeorgeShvab@users.noreply.github.com>
2021-10-09 19:35:12 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,11 @@
 window.onload = function () {
 	let open = false;
 	let isGridAnimationActive = false;
-	$(".nav__link-skills").click(() => {scrollTo("skills")});
-	$(".nav__link-about-me").click(() => {scrollTo("about-me")});
-	$(".nav__link-portfolio").click(() => {scrollTo("portfolio")});
 	$(window).scroll(() => {
 		scrollEvent("section__title-1", "section__title_is-active");
 		scrollEvent("section__title-2", "section__title_is-active");
 		scrollEvent("section__title-3", "section__title_is-active");
-		for (var i = 1; i < 15; i++) {
+		for (var i = 1; i < 16; i++) {
 			scrollEvent("grid__item" + "-" + i, "scroll-animation_active");
 		}
 		for (var a = 1; a < 7; a++) {
@@ -37,9 +34,6 @@ window.onload = function () {
 	$(".grid__item-10").click(() => {animate("grid__item-10")});
 	$(".grid__item-11").click(() => {animate("grid__item-11")});
 	$(".grid__item-12").click(() => {animate("grid__item-12")});
-	$(".grid__item-13").click(() => {animate("grid__item-13")});
-	$(".grid__item-14").click(() => {animate("grid__item-14")});
-	$(".grid__item-15").click(() => {animate("grid__item-15")});
 	function openMenu() {
 		$(".nav__list").addClass("nav__list_is-active");
 		$(".menu-button").addClass("menu-button_is-active");
@@ -54,14 +48,6 @@ window.onload = function () {
 		if ($("html").scrollTop() > $("." + className).offset().top - $(window).height() - 100) {
 			$("." + className).addClass(newClassName);
 		}
-	}
-	function scrollTo(className) {
-		$('html, body').animate({
-        	scrollTop: $('.' + className).offset().top
-    	}, 1500);
-    	if (open) {
-    		closeMenu();
-    	}
 	}
 	function animate(className) {
 		if (!isGridAnimationActive) {
