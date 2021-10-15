77eb74783d939b8a641408007c6ab7aa10f7a528
George Shvab <62070431+GeorgeShvab@users.noreply.github.com>
2021-10-16 00:32:45 +0300

    Add files via upload

--- works/konstruct/templates/scripts/script.js
+++ works/konstruct/templates/scripts/script.js
@@ -1,10 +1,26 @@
-window.onload = function() {	
+window.onload = function() {
+	let canClose = true;
+	let menuOpen = false;
 	$(".header__button").click(function() {
+		if (!menuOpen) {
+			openMenu();
+			canClose = false;
+			setTimeout(() => {canClose = true;}, 50);
+		}
+	});
+	$("section, header").click(function() {
+		if (menuOpen) {
+			if (canClose) {
+				closeMenu();
+			}
+		}
+	});
+	function openMenu() {
 		$(".header__nav-mobile").addClass("header__nav-mobile_active");
-		$("body").css("overflow", "hidden");
-	});
-	$(".menu__button").click(function() {
+		menuOpen = true;
+	}
+	function closeMenu() {
 		$(".header__nav-mobile").removeClass("header__nav-mobile_active");
-		$("body").css("overflow", "auto");
-	});
+		menuOpen = false;
+	}
 }
